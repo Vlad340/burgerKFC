0ba7a979ff1acb1ce3ad4a738621813f94705c03
Vlad340 <abcd-mail@yandex.ru>
2019-10-11 23:21:54 +0300

    units and decimal places added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,67 +1,68 @@
 public class Main {
     public static void main(String[] args) {
         Integer decimal = 10;
+        Integer hundredth=100;
 
-        Burger burger = new Burger();
-        burger.id = 2963L;
-        burger.name = "Шефбургер Джуниор Острый";
-        burger.calories = 250;
-        burger.weight=161;
-        burger.proteins=136;
-        burger.fats = 100;
-        burger.carbohydrates = 265;
-        burger.description = "Два сочных стрипса в острой панировке, сочные листья салата, " +
+        Burger burgerChefburger = new Burger();
+        burgerChefburger.id = 2963L;
+        burgerChefburger.name = "Шефбургер Джуниор Острый";
+        burgerChefburger.calories = 250;
+        burgerChefburger.weight=161;
+        burgerChefburger.proteins=136;
+        burgerChefburger.fats = 100;
+        burgerChefburger.carbohydrates = 265;
+        burgerChefburger.description = "Два сочных стрипса в острой панировке, сочные листья салата, " +
                 "пикантные маринованные огурчики, лук, фирменный соус «Бургер» и булочка с черно-белым кунжутом.";
-        burger.remarks = "* Данные по пищевой и энергетической ценности блюд, представленные в таблице, основаны на " +
+        burgerChefburger.remarks = "* Данные по пищевой и энергетической ценности блюд, представленные в таблице, основаны на " +
                 "данных лабораторных исследований, технико-технологических карт и данных по пищевой ценности, " +
                 "полученных от поставщиков. Различия в размерах порций, так же как региональные и сезонные различия " +
                 "могут влиять на пищевую ценность каждого продукта меню.<br/>Рецептура блюд периодически может меняться." +
                 " Информация предоставлена на основе текущего меню и может изменяться без предварительного уведомления." +
                 "** В некоторых ресторанах KFC продукты и цены могут отличаться";
-        burger.price = 99_00L;
-        burger.photoUrl = "https://s82079.cdn.ngenix.net/YLb95nAs4KMibLPRCtve6Je3.png?dw=230";
+        burgerChefburger.price = 99_00L;
+        burgerChefburger.photoUrl = "https://s82079.cdn.ngenix.net/YLb95nAs4KMibLPRCtve6Je3.png?dw=230";
 
-        System.out.println(burger.id);
-        System.out.println(burger.name);
-        System.out.println(burger.calories);
-        System.out.println(burger.weight);
-        System.out.println(burger.proteins/decimal);
-        System.out.println(burger.fats/decimal);
-        System.out.println(burger.carbohydrates/decimal);
-        System.out.println(burger.description);
-        System.out.println(burger.remarks);
-        System.out.println(burger.price);
-        System.out.println(burger.photoUrl);
+        System.out.println(burgerChefburger.id);
+        System.out.println(burgerChefburger.name);
+        System.out.println(burgerChefburger.calories+" Ккал");
+        System.out.println(burgerChefburger.weight+" г");
+        System.out.println((double)burgerChefburger.proteins/decimal+" г");
+        System.out.println((double)burgerChefburger.fats/decimal+" г");
+        System.out.println((double)burgerChefburger.carbohydrates/decimal+" г");
+        System.out.println(burgerChefburger.description);
+        System.out.println(burgerChefburger.remarks);
+        System.out.println((double) burgerChefburger.price/hundredth+" руб");
+        System.out.println(burgerChefburger.photoUrl);
 
-        Burger burger = new Burger();
-        burger.id = 2382L;
-        burger.name = "Чизбургер Де Люкс";
-        burger.calories = 193;
-        burger.weight=226;
-        burger.proteins=126;
-        burger.fats = 66;
-        burger.carbohydrates = 207;
-        burger.description = "Пряный горчичный соус, кетчуп, сочное филе в оригинальной панировке, лук, сыр Чеддер, " +
+        Burger burgerCheeseburger = new Burger();
+        burgerCheeseburger.id = 2382L;
+        burgerCheeseburger.name = "Чизбургер Де Люкс";
+        burgerCheeseburger.calories = 193;
+        burgerCheeseburger.weight=226;
+        burgerCheeseburger.proteins=126;
+        burgerCheeseburger.fats = 66;
+        burgerCheeseburger.carbohydrates = 207;
+        burgerCheeseburger.description = "Пряный горчичный соус, кетчуп, сочное филе в оригинальной панировке, лук, сыр Чеддер, " +
                 "огурцы на пшеничной булочке с кукурузной посыпкой, свежий салат и ломтики помидора.";
-        burger.remarks = "* Данные по пищевой и энергетической ценности блюд, представленные в таблице, основаны на " +
+        burgerCheeseburger.remarks = "* Данные по пищевой и энергетической ценности блюд, представленные в таблице, основаны на " +
                 "данных лабораторных исследований, технико-технологических карт и данных по пищевой ценности, " +
                 "полученных от поставщиков. Различия в размерах порций, так же как региональные и сезонные различия " +
                 "могут влиять на пищевую ценность каждого продукта меню.<br/>Рецептура блюд периодически может меняться." +
                 " Информация предоставлена на основе текущего меню и может изменяться без предварительного уведомления." +
                 "** В некоторых ресторанах KFC продукты и цены могут отличаться";
-        burger.price = 124_00L;
-        burger.photoUrl = "https://s82079.cdn.ngenix.net/MXahUbKht6Q6D2wGRYrAz7nu.png?dw=230";
+        burgerCheeseburger.price = 124_00L;
+        burgerCheeseburger.photoUrl = "https://s82079.cdn.ngenix.net/MXahUbKht6Q6D2wGRYrAz7nu.png?dw=230";
 
-        System.out.println(burger.id);
-        System.out.println(burger.name);
-        System.out.println(burger.calories);
-        System.out.println(burger.weight);
-        System.out.println(burger.proteins/decimal);
-        System.out.println(burger.fats/decimal);
-        System.out.println(burger.carbohydrates/decimal);
-        System.out.println(burger.description);
-        System.out.println(burger.remarks);
-        System.out.println(burger.price);
-        System.out.println(burger.photoUrl);
+        System.out.println(burgerCheeseburger.id);
+        System.out.println(burgerCheeseburger.name);
+        System.out.println(burgerCheeseburger.calories+" Ккал");
+        System.out.println(burgerCheeseburger.weight+" г");
+        System.out.println((double) burgerCheeseburger.proteins/decimal+" г");
+        System.out.println((double)burgerCheeseburger.fats/decimal+" г");
+        System.out.println((double)burgerCheeseburger.carbohydrates/decimal+" г");
+        System.out.println(burgerCheeseburger.description);
+        System.out.println(burgerCheeseburger.remarks);
+        System.out.println((double) burgerCheeseburger.price/hundredth+" руб");
+        System.out.println(burgerCheeseburger.photoUrl);
     }
 }
