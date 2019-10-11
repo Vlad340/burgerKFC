420161a0b011f25f6bbd8729196aaab0d3c0d004
Vlad340 <abcd-mail@yandex.ru>
2019-10-11 22:26:08 +0300

    add burger #1

--- src/main/java/Burger.java
+++ src/main/java/Burger.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Burger {
+    Long id;
+    String name;
+    Integer weight;
+    Integer calories;
+    Integer proteins;
+    Integer fats;
+    Integer carbohydrates;
+    String remarks;
+    String description;
+    Long price;
+    String photoUrl;
 }
